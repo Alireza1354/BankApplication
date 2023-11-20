a1d119bf17854c3d9359257b35f7a793bb5bc02f
Alex <karimfromlfco@gmail.com>
2023-11-20 09:48:59 +0330

    Make and seed account number

--- BankApplication/BankAccount.cs
+++ BankApplication/BankAccount.cs
@@ -12,10 +12,17 @@ namespace BankApplication
         public string Owner { get; set; }
         public decimal Balance { get; }
 
+        // It's private, which means it can only be accessed by code inside the BankAccount class.
+        // It's static, which means it's shared by all of the BankAccount objects.
+        // Every bank account has the same seed.
+        private static int accountNumberSeed = 1234567890;
+
         public BankAccount(string name, decimal initialBalance)
         {
             this.Owner = name;
             this.Balance = initialBalance;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
         }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
