2578b9b2f858796c373f17631d9ffb305a4ff437
Alex <karimfromlfco@gmail.com>
2023-11-20 13:12:19 +0330

    Make deposit & Make withdrawal

--- BankApplication/BankAccount.cs
+++ BankApplication/BankAccount.cs
@@ -11,6 +11,18 @@ namespace BankApplication
         public string Number { get; }
         public string Owner { get; set; }
         public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in transactions)
+                {
+                    //balance = balance + item.Amount;
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
 
         // It's private, which means it can only be accessed by code inside the BankAccount class.
         // It's static, which means it's shared by all of the BankAccount objects.
@@ -23,17 +35,35 @@ namespace BankApplication
         public BankAccount(string name, decimal initialBalance)
         {
             this.Owner = name;
-            this.Balance = initialBalance;
+
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
         }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive.");
+            }
+            var deposit = new Transaction(amount, date, note);
+            transactions.Add(deposit);
         }
 
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive.");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal.");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            transactions.Add(withdrawal);
         }
     }
 }
--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -4,9 +4,11 @@
     {
         static void Main(string[] args)
         {
-            var account = new BankAccount("Ali", 1000000);
+            var account = new BankAccount("Ali", 100000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
 
+            account.MakeWithdrawal(120, DateTime.Now, "Hammok");
+            Console.WriteLine(account.Balance);
         }
     }
 }
